8772d21bb60364383b5b9a7e736c51ecb5524583
Kees Verhaar <kees.verhaar@wigo4it.nl>
2025-09-24 10:57:33 +0200

    🕹️ Enable joystick emulation

--- keyboards/keychron/q5/ansi/keymaps/KeesV/keymap.c
+++ keyboards/keychron/q5/ansi/keymaps/KeesV/keymap.c
@@ -29,7 +29,13 @@ enum layers{
 };
 
 enum custom_keycodes {
-  MACRO_CMD_TAB = SAFE_RANGE
+  MACRO_CMD_TAB = SAFE_RANGE,
+  JOY_UP,
+  JOY_DOWN,
+  JOY_LEFT,
+  JOY_RIGHT,
+  JOY_BTN1,
+  JOY_BTN2
 };
 
 enum {
@@ -38,6 +44,11 @@ enum {
 
 tap_dance_action_t tap_dance_actions[] = {
     [TD_RSFT_CAPS] = ACTION_TAP_DANCE_DOUBLE(KC_RSFT, KC_CAPS)
+};
+
+joystick_config_t joystick_axes[JOYSTICK_AXIS_COUNT] = {
+    JOYSTICK_AXIS_VIRTUAL, // X-Axis
+    JOYSTICK_AXIS_VIRTUAL, // Y-Axis
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -49,12 +60,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,              KC_RSFT,  KC_UP,    KC_P1,    KC_P2,    KC_P3,
         KC_LCTL,  KC_LOPT,  KC_LCMD,                                MEH_T(KC_SPC),                          KC_RCMD,  MO(MAC_FN), KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_P0,    KC_PDOT,  KC_PENT),
     [MAC_FN] = LAYOUT_ansi_100(
-        MACRO_CMD_TAB,  KC_BRID,  KC_BRIU,     KC_NO,    KC_NO,    RM_VALD,  RM_VALU,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,    KC_VOLU,  _______,  _______,  _______,  _______,  _______,  RM_TOGG,
-        _______,        _______,  _______,     _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,    _______,  _______,            _______,  _______,  _______,  _______,
-        RM_TOGG,        RM_NEXT,  RM_VALU,     RM_HUEU,  RM_SATU,  RM_SPDU,  _______,  _______,  _______,  _______,  _______,  _______,    _______,  _______,            _______,  _______,  _______,
-        _______,        RM_PREV,  RM_VALD,     RM_HUED,  RM_SATD,  RM_SPDD,  _______,  _______,  _______,  _______,  _______,  _______,              _______,            _______,  _______,  _______,  _______,
-        _______,                  G(S(KC_4)),  _______,  _______,  _______,  _______,  NK_TOGG,  _______,  _______,  _______,  _______,              _______,  _______,  _______,  _______,  _______,
-        _______,        _______,  _______,                                   _______,                                _______,  _______,    _______,  _______,  _______,  _______,  _______,  QK_BOOT,  _______),
+        MACRO_CMD_TAB,  KC_BRID,  KC_BRIU,     KC_NO,    KC_NO,    RM_VALD,  RM_VALU,  KC_MPRV,  KC_MPLY,  KC_MNXT,   KC_MUTE,  KC_VOLD,    KC_VOLU,  _______,  _______,  _______,  _______,  _______,  RM_TOGG,
+        _______,        JOY_BTN1, JOY_BTN2,    _______,  _______,  _______,  _______,  _______,  _______,  _______,   _______,  _______,    _______,  _______,            _______,  _______,  _______,  _______,
+        RM_TOGG,        RM_NEXT,  RM_VALU,     RM_HUEU,  RM_SATU,  RM_SPDU,  _______,  _______,  JOY_UP,   _______,   _______,  _______,    _______,  _______,            _______,  _______,  _______,
+        _______,        RM_PREV,  RM_VALD,     RM_HUED,  RM_SATD,  RM_SPDD,  _______,  JOY_LEFT, JOY_DOWN, JOY_RIGHT, _______,  _______,              _______,            _______,  _______,  _______,  _______,
+        _______,                  G(S(KC_4)),  _______,  _______,  _______,  _______,  NK_TOGG,  _______,  _______,   _______,  _______,              _______,  _______,  _______,  _______,  _______,
+        _______,        _______,  _______,                                   _______,                                 _______,  _______,    _______,  _______,  _______,  _______,  _______,  QK_BOOT,  _______),
     [WIN_BASE] = LAYOUT_ansi_100(
         KC_ESC,   KC_BRID,  KC_BRIU,  _______,  _______,  RM_VALD,  RM_VALU,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,    KC_VOLU,  RM_NEXT,  KC_DEL,   KC_HOME,  KC_END,   KC_PGUP,  KC_PGDN,
         KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,    KC_EQL,   KC_BSPC,            KC_NUM,   KC_PSLS,  KC_PAST,  KC_PMNS,
@@ -85,7 +96,35 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         unregister_code(KC_TAB);
       }
       break;
+
+    case JOY_UP:
+      joystick_set_axis(1, record->event.pressed ? -127 : 0);
+      return false;
+    case JOY_DOWN:
+      joystick_set_axis(1, record->event.pressed ? 127 : 0);
+      return false;
+    case JOY_LEFT:
+      joystick_set_axis(0, record->event.pressed ? -127 : 0);
+      return false;
+    case JOY_RIGHT:
+      joystick_set_axis(0, record->event.pressed ? 127 : 0);
+      return false;
+    case JOY_BTN1:
+      if(record->event.pressed) {
+        register_joystick_button(0);
+      } else {
+        unregister_joystick_button(0);
+      }
+      return false;
+    case JOY_BTN2:
+      if(record->event.pressed) {
+        register_joystick_button(1);
+      } else {
+        unregister_joystick_button(1);
+      }
+      return false;
   }
+
   return true;
 }
 
